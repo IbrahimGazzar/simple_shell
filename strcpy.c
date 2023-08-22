eb1bc64990538bd8dcbe1da6ebe6203ec291852f
OMARISMAIL8 <104282398+OMARISMAIL8@users.noreply.github.com>
2023-08-22 20:57:19 +0300

    Create strcpy.c
    updated files

---
+++ strcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strcpy - copy one string into another
+ * @dest: pointer to the string to be copied into
+ * @src: the source of the string
+ *
+ * Return: the pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	for (src = 0; i != '\0'; i++)
+		dest[i] = src[i];
+	dest[i] = src[i];
+	return (*dest);
+}
