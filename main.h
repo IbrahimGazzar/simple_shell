6d4b1c53e4f718c5569180cfefaf06786f7c1d34
IbrahimGazzar <Ibrahim.m.Elgazzar@gmail.com>
2023-08-22 06:34:08 -0700

    Add function prototypes

--- main.h
+++ main.h
@@ -1,7 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-void shell(char*src);
-void sherror(char *src, char *str);
+void shell(char *src);
+void sherror(char *src, char *str, char **strarr);
+void strfree(char *str, char **strarr);
+int _strcmp(char *s1, char *s2);
+void strtrm(char *cmd);
+char **cmd_args(char *cmd);
 
 #endif
